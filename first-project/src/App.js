ae813d61ce15fce281d909c09f5e28b40a28161c
vadim-leonov <vadim.leonov@mgid.com>
2023-01-23 10:28:30 +0100

    add Feedback add FeedbackForm with test

---
+++ first-project/src/App.js
@@ -0,0 +1,15 @@
+import './App.scss';
+import Heading from "./Heading";
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Heading />
+      </header>
+    </div>
+  );
+}
+
+export default App;
